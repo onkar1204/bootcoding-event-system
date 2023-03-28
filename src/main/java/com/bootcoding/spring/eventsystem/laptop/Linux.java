5ae80542aeb68e25b2f58bf87deb33c56141c9ae
omkar1204 <omahirkar008@gmail.com>
2023-03-28 20:13:07 +0530

    OOPs

---
+++ src/main/java/com/bootcoding/spring/eventsystem/laptop/Linux.java
@@ -0,0 +1,10 @@
+package com.bootcoding.spring.eventsystem.laptop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Linux {
+    public Linux(){
+        System.out.println("I'm In Linux Class");
+    }
+}
