e6bbd64c62daeb3efe6168826312f2c98a1aaf08
omkar1204 <omahirkar008@gmail.com>
2023-03-29 14:03:30 +0530

    OOPs

--- src/main/java/com/bootcoding/spring/eventsystem/EventSystemApplication.java
+++ src/main/java/com/bootcoding/spring/eventsystem/EventSystemApplication.java
@@ -2,6 +2,7 @@ package com.bootcoding.spring.eventsystem;
 
 import com.bootcoding.spring.eventsystem.laptop.Laptop;
 import com.bootcoding.spring.eventsystem.marker.Marker;
+import com.bootcoding.spring.eventsystem.switch_board.SwitchBoard;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,12 +11,12 @@ import org.springframework.stereotype.Component;
 @Component
 @SpringBootApplication
 public class EventSystemApplication {
+//    @Autowired
+//    private Marker marker;
+//    @Autowired
+//    private Laptop laptop;
     @Autowired
-    private Marker marker;
-    @Autowired
-    private Laptop laptop;
-
-
+    private SwitchBoard switchBoard;
 
     public static void main(String[] args) {
         SpringApplication.run(EventSystemApplication.class, args);
